7d1d10ca6923d5a347b64e1ffe3bfc4a5707fe05
Guilherme Leobas <guilhermeleobas@gmail.com>
2016-01-27 22:56:20 +0000

    install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# install softwares
+brew install neovim
+brew install tmux
+brew install zsh
+
+brew install node
+brew install nmp
+
+brew install python3
+
+# create symbolic links
+ln -s nvim ~/.config/nvim/init.vim
+ln -s tmux.conf ~/.tmux.conf
+ln -s zshrc ~/.zshrc
